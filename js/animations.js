60e82b92b0d0fbb577e339fbc2d299c9424bef9b
Yannic Reust <yannic.reust@bluewin.ch>
2023-03-22 16:35:55 +0100

    wip level3 & refactor

--- js/animations.js
+++ js/animations.js
@@ -1,48 +1,3 @@
-const charAnim = charactersc2.getAnimations()[0];
-
-let lastDirection = "right";
-let atStart = true;
-let atEnd = false;
-let enableKeyboard = true;
-
-charactersc2.addEventListener("animationend", () => {
-  if (lastDirection === "right") atEnd = true;
-  if (lastDirection === "left") atStart = true;
-});
-
-const moveAll = function () {
-  clouds1Anim.play();
-  clouds2Anim.play();
-  background1Anim.play();
-  background2Anim.play();
-  ground1Anim.play();
-  ground2Anim.play();
-  infoGuyBoxAnim.play();
-  charAnim.play();
-};
-
-const reverseAll = function () {
-  clouds1Anim.reverse();
-  clouds2Anim.reverse();
-  background1Anim.reverse();
-  background2Anim.reverse();
-  ground1Anim.reverse();
-  ground2Anim.reverse();
-  infoGuyBoxAnim.reverse();
-  charAnim.reverse();
-};
-
-const pause = function () {
-  clouds1Anim.pause();
-  clouds2Anim.pause();
-  background1Anim.pause();
-  background2Anim.pause();
-  ground1Anim.pause();
-  ground2Anim.pause();
-  infoGuyBoxAnim.pause();
-  charAnim.pause();
-};
-
 const moveLeft = function () {
   checkColisonGuy();
   collisionHole();
@@ -90,30 +45,3 @@ const scoreDecrease = function () {
   score--;
 };
 
-//=====================================
-// Tastaturabfrage
-//=====================================
-
-globalThis.addEventListener("down", onkeydown, false);
-globalThis.addEventListener("up", onkeyup, false);
-
-let keys = {};
-onkeydown = onkeyup = (e) => {
-  pause();
-  if (enableKeyboard) {
-    keys[e.code] = e.type == "keydown";
-    if (keys.KeyD && keys.KeyW) jump(), moveRight();
-    else if (keys.KeyA && keys.KeyW) jump(), moveLeft();
-    else if (keys.KeyA && !keys.KeyD) moveLeft();
-    else if (keys.KeyD && !keys.KeyA) moveRight();
-    else if (keys.KeyW && !keys.KeyS) jump();
-    else if (keys.KeyA && keys.KeyD) pause();
-    else if (!keys.KeyA && !keys.KeyD) pause();
-  }
-};
-
-myFunc();
-function myFunc() {
-  myFunc = function () {};
-  pause();
-}
