a73383350ee26c176b058128df5e3ca026400b7c
Yannic-reust <62382435+Yannic-reust@users.noreply.github.com>
2023-06-18 10:51:48 +0200

    wip

--- js/configure.js
+++ js/configure.js
@@ -14,9 +14,17 @@ function ApiCall(e) {
   const res = JSON.parse(xhr.response).current.condition.text;
 
   if (res === "Sunny") {
+    document.getElementById("cloudPreview").src = '';
+    document.getElementById("textPreview").innerHTML = "sunny"
     console.log("sunny");
   } else if (res === "Overcast") {
     console.log("overcast");
+  }
+  else if (res === "Clear") {
+    console.log("clear");
+  }
+  else{
+    console.log(res);
   }
   document.getElementById("loadingSpinner").style.visibility = "hidden";
 }
